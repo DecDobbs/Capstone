10ac6df50739aaf7ae45ebab5f24a49dfec3439f
DecDobbs <115410289+DecDobbs@users.noreply.github.com>
2023-05-02 19:01:00 +0100

    Add files via upload

--- Program - Copy.cs
+++ Program - Copy.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System.Net.Security;
+using System.IO;
+using System.ComponentModel.Design;
+
+// Recognising i am pulling data from a saved file on the system, i must use System.IO to access these.
+
+namespace Capstone 
+{
+    public class Transactions 
+        // i create a public class for Transactions which will contain my IDs and Data for the model. This is where my primary Structs will be.
+    { 
+        public int projID { get; set; } 
+        // This will be for the ID segment of our Beige files
+        public string projType { get; set; }
+        // This will be for the Type segment of our Beige files
+        public float projAmount { get; set; }
+        // This will be for the Amount segment of our Beige files
+
+    }
+    public class Project 
+    {
+
+    }
+}
+
